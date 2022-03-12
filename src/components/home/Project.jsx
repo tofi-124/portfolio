642719535245b8fbc49ffb4f8b41cc628a54e5d7
tofi-124 <tofikahm@yahoo.com>
2022-03-12 16:47:38 -0500

    projects now showing on website

---
+++ src/components/home/Project.jsx
@@ -0,0 +1,103 @@
+import React, { useState, useEffect, useCallback } from "react";
+import Container from "react-bootstrap/Container";
+import Jumbotron from "react-bootstrap/Jumbotron";
+import Row from "react-bootstrap/Row";
+import ProjectCard from "./ProjectCard";
+import axios from "axios";
+
+const dummyProject = {
+  name: null,
+  description: null,
+  svn_url: null,
+  stargazers_count: null,
+  languages_url: null,
+  pushed_at: null,
+};
+const API = "https://api.github.com";
+// const gitHubQuery = "/repos?sort=updated&direction=desc";
+// const specficQuerry = "https://api.github.com/repos/hashirshoaeb/";
+
+const Project = ({ heading, username, length, specfic }) => {
+  const allReposAPI = `${API}/users/${username}/repos?sort=updated&direction=desc`;
+  const specficReposAPI = `${API}/repos/${username}`;
+  const dummyProjectsArr = new Array(length + specfic.length).fill(
+    dummyProject
+  );
+
+  const [projectsArray, setProjectsArray] = useState([]);
+
+  const fetchRepos = useCallback(async () => {
+    let repoList = [];
+    try {
+      // getting all repos
+      const response = await axios.get(allReposAPI);
+      // slicing to the length
+      response.data.every(repo => {
+        if (repo.name === "tweeter" ||
+          repo.name === "Resource-Wall" ||
+          repo.name === "tinyapp" ||
+          repo.name === "scheduler"
+        ) {
+          repoList.push(repo);
+        }
+        if (repoList.length === 4) {
+          return false;
+        } else {
+          return true;
+        }
+      });
+      // adding specified repos
+      try {
+        for (let repoName of specfic) {
+          const response = await axios.get(`${specficReposAPI}/${repoName}`);
+          repoList.push(response.data);
+        }
+      } catch (error) {
+        console.error(error.message);
+      }
+      // setting projectArray
+      // TODO: remove the duplication.
+      setProjectsArray(repoList);
+    } catch (error) {
+      console.error(error.message);
+    }
+  }, [allReposAPI, length, specfic, specficReposAPI]);
+
+  useEffect(() => {
+    fetchRepos();
+  }, [fetchRepos]);
+  const images = [
+    require('../../assets/repo-images/2.PNG'),
+    require('../../assets/repo-images/4.gif'),
+    require('../../assets/repo-images/1.gif'),
+    require('../../assets/repo-images/3.png'),
+  ];
+
+  return (
+    <Jumbotron fluid id="projects" className="bg-light m-0">
+      <Container className="">
+        <h2 className="display-4 pb-5 text-center">{heading}</h2>
+        <Row>
+          {projectsArray.length
+            ? projectsArray.map((project, index) => (
+              <ProjectCard
+                key={`project-card-${index}`}
+                id={`project-card-${index}`}
+                value={project}
+                image={images[index]}
+              />
+            ))
+            : dummyProjectsArr.map((project, index) => (
+              <ProjectCard
+                key={`dummy-${index}`}
+                id={`dummy-${index}`}
+                value={project}
+              />
+            ))}
+        </Row>
+      </Container>
+    </Jumbotron>
+  );
+};
+
+export default Project;
