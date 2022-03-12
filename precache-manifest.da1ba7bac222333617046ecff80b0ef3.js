892041553cd441d4438216b97a4cb380909781c1
tofi-124 <tofikahm@yahoo.com>
2022-03-12 17:17:34 -0500

    Updates

---
+++ precache-manifest.da1ba7bac222333617046ecff80b0ef3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8436d3d586b95fd8fb4498217db30920",
+    "url": "/Profolio/index.html"
+  },
+  {
+    "revision": "ad1d9cc877fe2fbfb746",
+    "url": "/Profolio/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "044fcbf2cbca9bfaecd5",
+    "url": "/Profolio/static/js/2.732463bb.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/Profolio/static/js/2.732463bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad1d9cc877fe2fbfb746",
+    "url": "/Profolio/static/js/main.e45264b8.chunk.js"
+  },
+  {
+    "revision": "07a3390ad6fbcb1de3a9",
+    "url": "/Profolio/static/js/runtime-main.18365ab1.js"
+  },
+  {
+    "revision": "6ea23c60c12c930d7f06d541e8401473",
+    "url": "/Profolio/static/media/1.6ea23c60.gif"
+  },
+  {
+    "revision": "72ce3eea8bd61d4c5e5ec68a4a328876",
+    "url": "/Profolio/static/media/2.72ce3eea.PNG"
+  },
+  {
+    "revision": "54ff5f236c0edaceb23aef19f09074c8",
+    "url": "/Profolio/static/media/3.54ff5f23.png"
+  },
+  {
+    "revision": "b08d8f21250512e3556507c98f711b57",
+    "url": "/Profolio/static/media/4.b08d8f21.gif"
+  },
+  {
+    "revision": "9f06960f092ee991f1262b3a157969e4",
+    "url": "/Profolio/static/media/tofikmohammed.9f06960f.jpg"
+  }
+]);
