d1e0eb3502fc219276c668550a4a956d41aaf0a5
tofi-124 <tofikahm@yahoo.com>
2022-03-17 12:46:53 -0400

    deploy error

--- pages.js
+++ pages.js
@@ -1,6 +1,6 @@
 const ghpages = require("gh-pages");
 const pathname = `${__dirname}/build`;
-const repoURL = "https://github.com/tofi-124/portfolio";
+const repoURL = "https://github.com/tofi-124/tofi-124.github.io.git";
 
 ghpages.publish(
   pathname,
